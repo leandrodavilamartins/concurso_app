5b7a8fb63377c3458ff1a1a0d80463a46d55797c
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2023-08-26 15:54:42 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import 'carbon-components-svelte/css/g90.css';
+	import { Link } from 'carbon-components-svelte';
+</script>
+
+<div class="container">
+	<nav>
+		<Link href="/translator">Translator</Link>
+	</nav>
+</div>
+<slot />
+
+<style>
+	.container {
+		display: flex;
+		margin: auto;
+		align-items: center;
+	}
+	nav {
+		display: flex;
+		margin: auto;
+	}
+</style>
