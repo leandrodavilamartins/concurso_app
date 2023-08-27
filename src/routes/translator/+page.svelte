640a507f611e1ff76e3bdc6d0f18e205173472a3
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2023-08-27 18:57:25 -0300

    systemMessage added

--- src/routes/translator/+page.svelte
+++ src/routes/translator/+page.svelte
@@ -1,20 +1,35 @@
 <script>
 	import { TextArea, Button, Loading } from 'carbon-components-svelte';
 	import { chat } from '$lib/api_init.client';
-	import { HumanMessage } from 'langchain/schema';
+	import { HumanMessage, SystemMessage } from 'langchain/schema';
 
 	$: text = '';
-	$: console.log(text);
+	$: translatedText = '';
+
+	let loaderOn = false;
 
 	async function callToApi() {
-		const response = await chat.predictMessages([new HumanMessage(`${text}`)]);
-		console.log(response);
+		loaderOn = true;
+		const response = await chat.predictMessages([
+			new SystemMessage('You are a helpful assistant that translates Portuguese to English'),
+			new HumanMessage(`${text}`)
+		]);
+		console.log(response.content);
+		translatedText = response.content;
+		loaderOn = false;
 	}
 </script>
 
 <div class="container">
 	<TextArea labelText="Texto" placeholder="Digite aqui o texto a ser traduzido" bind:value={text} />
 	<Button on:click={callToApi}>Enviar</Button>
+</div>
+{#if loaderOn}
+	<Loading />
+{/if}
+
+<div>
+	<p>{translatedText}</p>
 </div>
 
 <style>
